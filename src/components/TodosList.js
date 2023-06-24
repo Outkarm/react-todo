6f36029e6e79abe1c0c48a56a79c823d25c7fb30
Outkarm <jkpordje@gmail.com>
2023-06-24 11:04:08 +0000

    fix linters

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,37 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
-// eslint-disable-next-line
-const TodosList = (props) => {
-  const {
-    todos, handleChange, delTodo, setUpdate,
-  } = props;
 
-  return (
-    <ul>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          handleChange={handleChange}
-          delTodo={delTodo}
-          setUpdate={setUpdate}
-        />
-      ))}
-    </ul>
-  );
-};
+const TodosList = ({
+  todos, handleChange, delTodo, setUpdate,
+}) => (
+  <ul>
+    {todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        handleChange={handleChange}
+        delTodo={delTodo}
+        setUpdate={setUpdate}
+      />
+    ))}
+  </ul>
+);
 
 TodosList.propTypes = {
   todos: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string,
-      title: PropTypes.string,
-      completed: PropTypes.bool,
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
     }),
   ).isRequired,
   handleChange: PropTypes.func.isRequired,
   delTodo: PropTypes.func.isRequired,
   setUpdate: PropTypes.func.isRequired,
 };
+
 export default TodosList;
